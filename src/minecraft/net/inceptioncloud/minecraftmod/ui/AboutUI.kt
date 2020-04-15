1f227d5adf7a73c5d09b5e6615981e5beff10c4e
incxption <58042339+incxption@users.noreply.github.com>
2020-04-15 20:22:54 +0200

    Minimum width for the About screen
    The method that was created (GuiScreen.drawSizeNotSupported) can also be used by other GUIs in the future.
    
    #4pknkt[committed]

--- src/minecraft/net/inceptioncloud/minecraftmod/ui/AboutUI.kt
+++ src/minecraft/net/inceptioncloud/minecraftmod/ui/AboutUI.kt
@@ -35,96 +35,102 @@ class AboutUI(val parentScreen: GuiScreen) : GuiScreen()
      */
     override fun drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float)
     {
-        Gui.drawRect(0, 0, width, height, BluePalette.BACKGROUND.rgb)
+        if (mc.displayWidth >= 400 && mc.displayHeight >= 400)
+        {
+            Gui.drawRect(0, 0, width, height, BluePalette.BACKGROUND.rgb)
 
-        val wHeight = (height / 1.3).toInt()
-        val wWidth = (wHeight / 1.2).toInt()
-        val wX = width / 2 - wWidth / 2
-        val wY = height / 2 - wHeight / 2
+            val wHeight = (height / 1.3).toInt()
+            val wWidth = (wHeight / 1.2).toInt()
+            val wX = width / 2 - wWidth / 2
+            val wY = height / 2 - wHeight / 2
 
-        val header = wHeight / 11
-        val body = wHeight - header
-        val fontRenderer = InceptionMod.getInstance().fontDesign.retrieveOrBuild(" Medium", (header * 1.1).toInt())
+            val header = wHeight / 11
+            val body = wHeight - header
+            val fontRenderer = InceptionMod.getInstance().fontDesign.retrieveOrBuild(" Medium", (header * 1.1).toInt())
 
-        // Header
-        RenderUtils.drawRoundRect(wX, wY, wWidth, header, 100, BluePalette.PRIMARY)
-        Gui.drawRect(wX, wY + header / 2, wX + wWidth, wY + header, BluePalette.PRIMARY.rgb)
-        fontRenderer.drawCenteredString("About", wX + wWidth / 2, wY + header / 2 - fontRenderer.height / 4,
-                BluePalette.FOREGROUND.rgb, false)
+            // Header
+            RenderUtils.drawRoundRect(wX, wY, wWidth, header, 100, BluePalette.PRIMARY)
+            Gui.drawRect(wX, wY + header / 2, wX + wWidth, wY + header, BluePalette.PRIMARY.rgb)
+            fontRenderer.drawCenteredString("About", wX + wWidth / 2, wY + header / 2 - fontRenderer.height / 4,
+                    BluePalette.FOREGROUND.rgb, false)
 
-        // Body
-        RenderUtils.drawRoundRect(wX, wY + body, wWidth, header, 100, BluePalette.FOREGROUND)
-        Gui.drawRect(wX, wY + header, wX + wWidth, wY + wHeight - header / 2, BluePalette.FOREGROUND.rgb)
+            // Body
+            RenderUtils.drawRoundRect(wX, wY + body, wWidth, header, 100, BluePalette.FOREGROUND)
+            Gui.drawRect(wX, wY + header, wX + wWidth, wY + wHeight - header / 2, BluePalette.FOREGROUND.rgb)
 
-        //region Content
-        val titleFont = InceptionMod.getInstance().fontDesign.retrieveOrBuild("", (header * 1.15).toInt())
-        val largeFont = InceptionMod.getInstance().fontDesign.retrieveOrBuild("", header / 1)
-        val smallFont = InceptionMod.getInstance().fontDesign.retrieveOrBuild("", (header / 1.5).toInt())
-        val largeColor = 0x545454
-        val smallColor = 0x6D6D6D
-        val imageSize = (smallFont.height * 1.2).toInt()
-        var textY = wY + header + 10
+            //region Content
+            val titleFont = InceptionMod.getInstance().fontDesign.retrieveOrBuild("", (header * 1.15).toInt())
+            val largeFont = InceptionMod.getInstance().fontDesign.retrieveOrBuild("", header / 1)
+            val smallFont = InceptionMod.getInstance().fontDesign.retrieveOrBuild("", (header / 1.5).toInt())
+            val largeColor = 0x545454
+            val smallColor = 0x6D6D6D
+            val imageSize = (smallFont.height * 1.2).toInt()
+            var textY = wY + header + 10
 
-        //region Title
-        titleFont.drawCenteredString("Inception Cloud Minecraft Mod", wX + wWidth / 2, textY, largeColor, false)
-        textY += titleFont.height + 2
-        smallFont.drawCenteredString(InceptionMod.getVersion(), wX + wWidth / 2, textY, smallColor, false)
-        textY += smallFont.height + 12
-        //endregion
+            //region Title
+            titleFont.drawCenteredString("Inception Cloud Minecraft Mod", wX + wWidth / 2, textY, largeColor, false)
+            textY += titleFont.height + 2
+            smallFont.drawCenteredString(InceptionMod.getVersion(), wX + wWidth / 2, textY, smallColor, false)
+            textY += smallFont.height + 12
+            //endregion
 
-        //region Network
-        largeFont.drawString("part of the Inception Cloud Network", wX + 10F, textY.toFloat(), largeColor, false)
-        textY += largeFont.height + 2
+            //region Network
+            largeFont.drawString("part of the Inception Cloud Network", wX + 10F, textY.toFloat(), largeColor, false)
+            textY += largeFont.height + 2
 
-        smallFont.drawString("visit http://inceptioncloud.net for more information", wX + 10F, textY.toFloat(), smallColor, false)
-        textY += smallFont.height + 1
+            smallFont.drawString("visit http://inceptioncloud.net for more information", wX + 10F, textY.toFloat(), smallColor, false)
+            textY += smallFont.height + 1
 
-        drawIcon("twitter", wX + 20, textY, imageSize)
-        smallFont.drawString("@inceptioncloud", wX + 22F + imageSize, textY + 3F, smallColor, false)
-        textY += imageSize + 2
+            drawIcon("twitter", wX + 20, textY, imageSize)
+            smallFont.drawString("@inceptioncloud", wX + 22F + imageSize, textY + 3F, smallColor, false)
+            textY += imageSize + 2
 
-        drawIcon("instagram", wX + 20, textY, imageSize)
-        smallFont.drawString("@inceptioncloud", wX + 22F + imageSize, textY + 3F, smallColor, false)
-        textY += imageSize + 2
+            drawIcon("instagram", wX + 20, textY, imageSize)
+            smallFont.drawString("@inceptioncloud", wX + 22F + imageSize, textY + 3F, smallColor, false)
+            textY += imageSize + 2
 
-        drawIcon("discord", wX + 20, textY, imageSize)
-        smallFont.drawString("https://discord.gg/DJRb4fF", wX + 22F + imageSize, textY + 3F, smallColor, false)
-        textY += 20
-        //endregion
+            drawIcon("discord", wX + 20, textY, imageSize)
+            smallFont.drawString("https://discord.gg/DJRb4fF", wX + 22F + imageSize, textY + 3F, smallColor, false)
+            textY += 20
+            //endregion
 
-        //region Developer
-        drawIcon("heart", wX + 10 + largeFont.getStringWidth("developed with"), textY - (imageSize / 4), (imageSize * 1.5).toInt())
-        largeFont.drawString("developed with", wX + 10F, textY.toFloat(), largeColor, false)
-        largeFont.drawString("by inception", (wX + 11F + (imageSize * 1.5) + largeFont.getStringWidth("developed with"))
-            .toFloat(), textY.toFloat(), largeColor, false)
-        textY += largeFont.height + 2
+            //region Developer
+            drawIcon("heart", wX + 10 + largeFont.getStringWidth("developed with"), textY - (imageSize / 4), (imageSize * 1.5).toInt())
+            largeFont.drawString("developed with", wX + 10F, textY.toFloat(), largeColor, false)
+            largeFont.drawString("by inception", (wX + 11F + (imageSize * 1.5) + largeFont.getStringWidth("developed with"))
+                .toFloat(), textY.toFloat(), largeColor, false)
+            textY += largeFont.height + 2
 
-        drawIcon("twitter", wX + 20, textY, imageSize)
-        smallFont.drawString("@theincxption", wX + 22F + imageSize, textY + 3F, smallColor, false)
-        textY += imageSize + 2
+            drawIcon("twitter", wX + 20, textY, imageSize)
+            smallFont.drawString("@theincxption", wX + 22F + imageSize, textY + 3F, smallColor, false)
+            textY += imageSize + 2
 
-        drawIcon("instagram", wX + 20, textY, imageSize)
-        smallFont.drawString("@theincxption", wX + 22F + imageSize, textY + 3F, smallColor, false)
-        textY += imageSize + 2
+            drawIcon("instagram", wX + 20, textY, imageSize)
+            smallFont.drawString("@theincxption", wX + 22F + imageSize, textY + 3F, smallColor, false)
+            textY += imageSize + 2
 
-        drawIcon("github", wX + 20, textY, imageSize)
-        smallFont.drawString("@theincxption", wX + 22F + imageSize, textY + 3F, smallColor, false)
-        textY += 20
-        //endregion
+            drawIcon("github", wX + 20, textY, imageSize)
+            smallFont.drawString("@theincxption", wX + 22F + imageSize, textY + 3F, smallColor, false)
+            textY += 20
+            //endregion
 
-        //region Credits
-        largeFont.drawString("credits", wX + 10F, textY.toFloat(), largeColor, false)
-        textY += largeFont.height + 2
+            //region Credits
+            largeFont.drawString("credits", wX + 10F, textY.toFloat(), largeColor, false)
+            textY += largeFont.height + 2
 
-        smallFont.drawString("icons by https://icons8.com", wX + 20F, textY + 3F, smallColor, false)
-        textY += smallFont.height + 2
+            smallFont.drawString("icons by https://icons8.com", wX + 20F, textY + 3F, smallColor, false)
+            textY += smallFont.height + 2
 
-        smallFont.drawString("fonts by https://fonts.google.com", wX + 20F, textY + 3F, smallColor, false)
-        //endregion
+            smallFont.drawString("fonts by https://fonts.google.com", wX + 20F, textY + 3F, smallColor, false)
+            //endregion
 
-        //endregion
+            //endregion
 
-        super.drawScreen(mouseX, mouseY, partialTicks)
+            super.drawScreen(mouseX, mouseY, partialTicks)
+        } else
+        {
+            drawSizeNotSupported()
+        }
     }
 
     /**
