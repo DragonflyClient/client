f7871709e9600aa33b44e292965bcb865240c34d
moritz <theincxption@gmail.com>
2020-06-06 15:43:18 +0200

    Added the documentation for the FontRendererBuilder

--- src/minecraft/net/inceptioncloud/minecraftmod/engine/font/FontRendererBuilder.kt
+++ src/minecraft/net/inceptioncloud/minecraftmod/engine/font/FontRendererBuilder.kt
@@ -1,11 +1,24 @@
 package net.inceptioncloud.minecraftmod.engine.font
 
+/**
+ * ## Font Renderer Builder
+ *
+ * Specify preferences for building a font renderer based on a [WidgetFont]
+ *
+ * @param fontWeight the font weight ([FontWeight.REGULAR] by default)
+ * @param size the size of the font (19 by default)
+ * @param letterSpacing optional modification of the letter spacing (0.0 by default)
+ * @param forceCreation true, if no cached font renderer should be used
+ */
 data class FontRendererBuilder(
     var fontWeight: FontWeight,
     var size: Int,
     var letterSpacing: Double,
     var forceCreation: Boolean = false
 ) {
+    /**
+     * Default equals() implementation to compare builder preferences.
+     */
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -20,6 +33,9 @@ data class FontRendererBuilder(
         return true
     }
 
+    /**
+     * Default hashCode() implementation
+     */
     override fun hashCode(): Int {
         var result = fontWeight.hashCode()
         result = 31 * result + size
