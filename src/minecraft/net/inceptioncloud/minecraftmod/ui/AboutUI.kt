bbe9c75ce96ebd64b650cbfba8673fa85bdfd917
moritz <theincxption@gmail.com>
2020-06-06 18:15:46 +0200

    About UI is now using asynchronous font renderer productions

--- src/minecraft/net/inceptioncloud/minecraftmod/ui/AboutUI.kt
+++ src/minecraft/net/inceptioncloud/minecraftmod/ui/AboutUI.kt
@@ -2,9 +2,9 @@ package net.inceptioncloud.minecraftmod.ui
 
 import net.inceptioncloud.minecraftmod.Dragonfly
 import net.inceptioncloud.minecraftmod.design.color.BluePalette
+import net.inceptioncloud.minecraftmod.engine.font.FontWeight
 import net.inceptioncloud.minecraftmod.ui.components.button.ImageButton
 import net.inceptioncloud.minecraftmod.ui.renderer.RenderUtils
-import net.minecraft.client.gui.Gui
 import net.minecraft.client.gui.GuiButton
 import net.minecraft.client.gui.GuiScreen
 import net.minecraft.client.renderer.GlStateManager
@@ -18,26 +18,31 @@ import net.minecraft.util.ResourceLocation
  *
  * @property parentScreen the screen from which the UI was opened
  */
-class AboutUI(val parentScreen: GuiScreen) : GuiScreen()
-{
+class AboutUI(val parentScreen: GuiScreen) : GuiScreen() {
     /**
      * Adds the buttons (and other controls) to the screen in question. Called when the GUI is displayed and when the
      * window resizes, the buttonList is cleared beforehand.
      */
-    override fun initGui()
-    {
+    override fun initGui() {
         val iconSize = width.coerceAtMost(height) / 15
-        buttonList.add(ImageButton(1, 5, height - 5 - iconSize, iconSize, iconSize, ResourceLocation("inceptioncloud/icons/about/back.png")))
+        buttonList.add(
+            ImageButton(
+                1,
+                5,
+                height - 5 - iconSize,
+                iconSize,
+                iconSize,
+                ResourceLocation("inceptioncloud/icons/about/back.png")
+            )
+        )
     }
 
     /**
      * Draws the screen and all the components in it. Args : mouseX, mouseY, renderPartialTicks
      */
-    override fun drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float)
-    {
-        if (mc.displayWidth >= 400 && mc.displayHeight >= 400)
-        {
-            Gui.drawRect(0, 0, width, height, BluePalette.BACKGROUND.rgb)
+    override fun drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float) {
+        if (mc.displayWidth >= 400 && mc.displayHeight >= 400) {
+            drawRect(0, 0, width, height, BluePalette.BACKGROUND.rgb)
 
             val wHeight = (height / 1.3).toInt()
             val wWidth = (wHeight / 1.2).toInt()
@@ -46,22 +51,32 @@ class AboutUI(val parentScreen: GuiScreen) : GuiScreen()
 
             val header = wHeight / 11
             val body = wHeight - header
-            val fontRenderer = Dragonfly.fontDesign.retrieveOrBuild(" Medium", (header * 1.1).toInt())
+            val fontRenderer = Dragonfly.fontDesign.defaultFont.fontRendererAsync {
+                fontWeight = FontWeight.MEDIUM
+                size = (header * 1.1).toInt()
+            }
 
             // Header
             RenderUtils.drawRoundRect(wX, wY, wWidth, header, 100, BluePalette.PRIMARY)
-            Gui.drawRect(wX, wY + header / 2, wX + wWidth, wY + header, BluePalette.PRIMARY.rgb)
-            fontRenderer.drawCenteredString("About", wX + wWidth / 2, wY + header / 2 - fontRenderer.height / 4,
-                    BluePalette.FOREGROUND.rgb, false)
+            drawRect(wX, wY + header / 2, wX + wWidth, wY + header, BluePalette.PRIMARY.rgb)
+
+            fontRenderer?.drawCenteredString(
+                "About", wX + wWidth / 2, wY + header / 2 - fontRenderer.height / 4,
+                BluePalette.FOREGROUND.rgb, false
+            )
 
             // Body
             RenderUtils.drawRoundRect(wX, wY + body, wWidth, header, 100, BluePalette.FOREGROUND)
-            Gui.drawRect(wX, wY + header, wX + wWidth, wY + wHeight - header / 2, BluePalette.FOREGROUND.rgb)
+            drawRect(wX, wY + header, wX + wWidth, wY + wHeight - header / 2, BluePalette.FOREGROUND.rgb)
 
             //region Content
-            val titleFont = Dragonfly.fontDesign.retrieveOrBuild("", (header * 1.15).toInt())
-            val largeFont = Dragonfly.fontDesign.retrieveOrBuild("", header / 1)
-            val smallFont = Dragonfly.fontDesign.retrieveOrBuild("", (header / 1.5).toInt())
+            val titleFont = Dragonfly.fontDesign.defaultFont.fontRendererAsync { size = (header * 1.15).toInt() }
+            val largeFont = Dragonfly.fontDesign.defaultFont.fontRendererAsync { size = (header / 1) }
+            val smallFont = Dragonfly.fontDesign.defaultFont.fontRendererAsync { size = (header / 1.5).toInt() }
+
+            if (titleFont == null || largeFont == null || smallFont == null)
+                return
+
             val largeColor = 0x545454
             val smallColor = 0x6D6D6D
             val imageSize = (smallFont.height * 1.2).toInt()
@@ -101,10 +116,17 @@ class AboutUI(val parentScreen: GuiScreen) : GuiScreen()
             //endregion
 
             //region Developer
-            drawIcon("heart", wX + 10 + largeFont.getStringWidth("developed with"), textY - (imageSize / 4), (imageSize * 1.5).toInt())
+            drawIcon(
+                "heart",
+                wX + 10 + largeFont.getStringWidth("developed with"),
+                textY - (imageSize / 4),
+                (imageSize * 1.5).toInt()
+            )
             largeFont.drawString("developed with", wX + 10F, textY.toFloat(), largeColor, false)
-            largeFont.drawString("by inception", (wX + 11F + (imageSize * 1.5) + largeFont.getStringWidth("developed with"))
-                .toFloat(), textY.toFloat(), largeColor, false)
+            largeFont.drawString(
+                "by inception", (wX + 11F + (imageSize * 1.5) + largeFont.getStringWidth("developed with"))
+                    .toFloat(), textY.toFloat(), largeColor, false
+            )
             textY += largeFont.height + 2
 
             drawIcon("twitter", wX + 20, textY, imageSize)
@@ -133,8 +155,7 @@ class AboutUI(val parentScreen: GuiScreen) : GuiScreen()
             //endregion
 
             super.drawScreen(mouseX, mouseY, partialTicks)
-        } else
-        {
+        } else {
             drawSizeNotSupported()
         }
     }
@@ -142,20 +163,17 @@ class AboutUI(val parentScreen: GuiScreen) : GuiScreen()
     /**
      * A quick method that draws a icon from the "inceptioncloud/icon/about/" folder into the gui.
      */
-    fun drawIcon(name: String, x: Int, y: Int, size: Int)
-    {
+    private fun drawIcon(name: String, x: Int, y: Int, size: Int) {
         mc.textureManager.bindTexture(ResourceLocation("inceptioncloud/icons/about/$name.png"))
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F)
-        Gui.drawModalRectWithCustomSizedTexture(x, y, 0F, 0F, size, size, size.toFloat(), size.toFloat())
+        drawModalRectWithCustomSizedTexture(x, y, 0F, 0F, size, size, size.toFloat(), size.toFloat())
     }
 
     /**
      * Called by the controls from the buttonList when activated. (Mouse pressed for buttons)
      */
-    override fun actionPerformed(button: GuiButton?)
-    {
-        if (button?.id == 1)
-        {
+    override fun actionPerformed(button: GuiButton?) {
+        if (button?.id == 1) {
             mc.displayGuiScreen(parentScreen)
         }
     }
