22b1c474dc8ad524a5fc04a6b4f28f96b6fff63e
moritz <theincxption@gmail.com>
2020-07-04 20:15:32 +0200

    Moved META-INF and assets to resources source set

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,13 +14,27 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
+    implementation(fileTree("libraries"))
+    implementation(fileTree("libraries-minecraft"))
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.freeCompilerArgs += "-Xuse-experimental=kotlin.Experimental"
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    compileTestJava {
+        options.encoding = "UTF-8"
     }
 }
