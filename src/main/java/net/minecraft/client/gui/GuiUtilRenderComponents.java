045ddc04e816044e91eb560371090f5bb066e22b
moritz <theincxption@gmail.com>
2020-07-17 21:25:14 +0200

    Format GuiUtilRenderComponents

--- src/main/java/net/minecraft/client/gui/GuiUtilRenderComponents.java
+++ src/main/java/net/minecraft/client/gui/GuiUtilRenderComponents.java
@@ -14,31 +14,28 @@ public class GuiUtilRenderComponents {
      * Remove the formatting colors from the text if it was configured in the chat settings and it
      * the color isn't forced.
      *
-     * @param text The text
+     * @param text       The text
      * @param forceColor True to always keep the color, false to use the value from the chat settings
      * @return The result
      */
-    public static String removeTextColorsIfConfigured (String text, boolean forceColor)
-    {
+    public static String removeTextColorsIfConfigured(String text, boolean forceColor) {
         return !forceColor && !Minecraft.getMinecraft().gameSettings.chatColours ? EnumChatFormatting.getTextWithoutFormattingCodes(text) : text;
     }
 
-    public static List<IChatComponent> splitText (IChatComponent original, int maxTextLenght, IFontRenderer fontRenderer /* NOTE: This parameter is unnecessary because the Minecraft Font Renderer is used to split the text */,
-                                                  boolean trimSpace, boolean forceTextColor)
-    {
+    public static List<IChatComponent> splitText(
+            IChatComponent original, int maxTextLength, IFontRenderer fontRenderer, boolean trimSpace, boolean forceTextColor
+    ) {
         int i = 0;
         IChatComponent currentComponent = new ChatComponentText("");
         List<IChatComponent> newList = Lists.newArrayList();
         List<IChatComponent> listWithOriginal = Lists.newArrayList(original);
 
-        for (int j = 0 ; j < listWithOriginal.size(); ++j)
-        {
+        for (int j = 0; j < listWithOriginal.size(); ++j) {
             IChatComponent ichatcomponent1 = listWithOriginal.get(j);
             String s = ichatcomponent1.getUnformattedTextForChat();
             boolean flag = false;
 
-            if (s.contains("\n"))
-            {
+            if (s.contains("\n")) {
                 int k = s.indexOf(10);
                 String s1 = s.substring(k + 1);
                 s = s.substring(0, k + 1);
@@ -54,28 +51,22 @@ public class GuiUtilRenderComponents {
             ChatComponentText chatcomponenttext1 = new ChatComponentText(s5);
             chatcomponenttext1.setChatStyle(ichatcomponent1.getChatStyle().createShallowCopy());
 
-            if (i + i1 > maxTextLenght)
-            {
-                String s2 = fontRenderer.trimStringToWidth(s4, maxTextLenght - i);
+            if (i + i1 > maxTextLength) {
+                String s2 = fontRenderer.trimStringToWidth(s4, maxTextLength - i);
                 String s3 = s2.length() < s4.length() ? s4.substring(s2.length()) : null;
 
-                if (s3 != null)
-                {
+                if (s3 != null) {
                     int l = s2.lastIndexOf(" ");
 
-                    if (l >= 0 && fontRenderer.getStringWidth(s4.substring(0, l)) > 0)
-                    {
+                    if (l >= 0 && fontRenderer.getStringWidth(s4.substring(0, l)) > 0) {
                         s2 = s4.substring(0, l);
 
-                        if (trimSpace)
-                        {
+                        if (trimSpace) {
                             ++l;
                         }
 
                         s3 = s4.substring(l);
-                    }
-                    else if (i > 0 && !s4.contains(" "))
-                    {
+                    } else if (i > 0 && !s4.contains(" ")) {
                         s2 = "";
                         s3 = s4;
                     }
@@ -91,18 +82,14 @@ public class GuiUtilRenderComponents {
                 flag = true;
             }
 
-            if (i + i1 <= maxTextLenght)
-            {
+            if (i + i1 <= maxTextLength) {
                 i += i1;
                 currentComponent.appendSibling(chatcomponenttext1);
-            }
-            else
-            {
+            } else {
                 flag = true;
             }
 
-            if (flag)
-            {
+            if (flag) {
                 newList.add(currentComponent);
                 i = 0;
                 currentComponent = new ChatComponentText("");
