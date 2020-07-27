97a38c6391c8e3d225e6cc55dfd8865da59833b8
Verity <58147038+verityyt@users.noreply.github.com>
2020-07-27 19:51:36 +0200

    Temporary remove modifier feature of hotkeys (coming soon) and finished single hotkey feature (no modifier)

--- src/main/java/net/inceptioncloud/dragonfly/hotkeys/HotkeyController.kt
+++ src/main/java/net/inceptioncloud/dragonfly/hotkeys/HotkeyController.kt
@@ -11,59 +11,43 @@ object HotkeyController {
     @JvmStatic
     val hotkeys = mutableListOf<Hotkey>()
 
-    private val pressedKeys = HashMap<Hotkey, List<Int>>()
+    val blockedHotkeys = mutableListOf<Hotkey>()
 
     init {
         println("Initializing hotkeys...")
         val test = HotkeyTypeChat(
             Keyboard.KEY_G,
-            Keyboard.KEY_LSHIFT,
+            null,
+            //Keyboard.KEY_LSHIFT,
             1.0,
             10.0,
             Color(0x26de81),
-            true,
+            false,
             "Mitte Safe, alles Safe"
         )
         hotkeys.add(test)
-        pressedKeys.put(test, listOf(-1, -1))
     }
 
     @JvmStatic
     fun updateKeys() {
 
         for (hotkey in hotkeys) {
-            if (hotkey.modifierKey == null) {
-                // Primary key only
-            } else {
 
-                val primary = hotkey.key
-                val secondary = hotkey.modifierKey
+            val primary = hotkey.key
+            val secondary = hotkey.modifierKey
 
-                if (secondary == Keyboard.KEY_LSHIFT && GuiScreen.isShiftKeyDown && !primary.isKeyPressed()) {
-                    pressedKeys[hotkey] = listOf(secondary, -1)
-                }
-
-                if (secondary == Keyboard.KEY_LSHIFT && GuiScreen.isShiftKeyDown && primary.isKeyPressed()) {
-                    val old = pressedKeys[hotkey]
-                    pressedKeys[hotkey] = listOf(old!![0], primary)
-                }
-
-                if (pressedKeys[hotkey]!![0] == secondary && pressedKeys[hotkey]!![0] == primary) {
-                    if (secondary == Keyboard.KEY_LSHIFT && GuiScreen.isShiftKeyDown && primary.isKeyPressed()) {
-
-                        if (hotkey is HotkeyTypeChat) {
-                            hotkey.direction = 1
-                        }
-
-                    } else {
-
-                        if (hotkey is HotkeyTypeChat && !hotkey.transition.isAtStart) {
+            if (secondary == null) {
+                if (primary.isKeyPressed()) {
+                    if (hotkey is HotkeyTypeChat && !blockedHotkeys.contains(hotkey)) {
+                        hotkey.direction = 1
+                    }
+                } else {
+                    if (hotkey is HotkeyTypeChat) {
+                        if (!hotkey.transition.isAtStart) {
                             hotkey.direction = -1
                         }
-
                     }
                 }
-
             }
         }
     }
--- src/main/java/net/inceptioncloud/dragonfly/hotkeys/types/HotkeyTypeChat.kt
+++ src/main/java/net/inceptioncloud/dragonfly/hotkeys/types/HotkeyTypeChat.kt
@@ -1,14 +1,12 @@
 package net.inceptioncloud.dragonfly.hotkeys.types
 
 import net.inceptioncloud.dragonfly.hotkeys.Hotkey
+import net.inceptioncloud.dragonfly.hotkeys.HotkeyController
 import net.inceptioncloud.dragonfly.transition.number.SmoothDoubleTransition
-import net.inceptioncloud.dragonfly.transition.supplier.ForwardBackward
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.Gui
-import net.minecraft.client.gui.GuiNewChat
 import net.minecraft.client.gui.GuiScreen.Companion.sendChatMessage
 import net.minecraft.client.gui.ScaledResolution
-import org.lwjgl.input.Keyboard
 import java.awt.Color
 import java.util.*
 
@@ -16,7 +14,7 @@ class HotkeyTypeChat(
     override val key: Int,
     override val modifierKey: Int?,
     override val time: Double,
-    override val delay: Double,
+    override val delay: Double, // Only active if 'fadeOut' is 'false'
     override val color: Color,
     val fadeOut: Boolean,
     val message: String
@@ -25,6 +23,7 @@ class HotkeyTypeChat(
 
     var drawLine = true
     var direction = 0
+    lateinit var asyncDelay: Thread
 
     var transition = SmoothDoubleTransition.builder()
         .fadeIn(0).stay((time * 180).toInt()).fadeOut((time * 20).toInt())
@@ -35,22 +34,43 @@ class HotkeyTypeChat(
 
     override fun actionPerformed() {
         sendChatMessage(message, false)
+        HotkeyController.blockedHotkeys.add(this)
 
         if (!fadeOut) {
-            transition.destroy()
-            drawLine = false
-
-            Timer().schedule(object : TimerTask() {
-                override fun run() {
-                    transition = SmoothDoubleTransition.builder()
-                        .fadeIn(0).stay((time * 180).toInt()).fadeOut((time * 20).toInt())
-                        .start(0.0).end(1.0)
-                        .autoTransformator { direction }
-                        .reachEnd { actionPerformed() }
-                        .build()
-                    drawLine = true
+            activateDelay()
+        } else {
+            asyncDelay = Thread {
+                while (true) {
+                    if (transition.isAtStart) {
+                        activateDelay()
+                    }
                 }
-            }, (delay * 1000).toLong())
+            }
+
+            asyncDelay.start()
+        }
+    }
+
+    fun activateDelay() {
+        transition.destroy()
+        drawLine = false
+
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                transition = SmoothDoubleTransition.builder()
+                    .fadeIn(0).stay((time * 180).toInt()).fadeOut((time * 20).toInt())
+                    .start(0.0).end(1.0)
+                    .autoTransformator { direction }
+                    .reachEnd { actionPerformed() }
+                    .build()
+                drawLine = true
+                HotkeyController.blockedHotkeys.remove(this@HotkeyTypeChat)
+            }
+        }, (delay * 1000).toLong())
+
+        if (fadeOut) {
+            asyncDelay.stop()
+            asyncDelay.interrupt()
         }
     }
 
