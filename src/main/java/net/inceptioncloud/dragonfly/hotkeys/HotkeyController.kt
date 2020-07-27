b2340cf0b668173dc387573fa75dafc7c6b7c38a
Verity <58147038+verityyt@users.noreply.github.com>
2020-07-27 14:37:43 +0200

    Save which key is pressed first

--- src/main/java/net/inceptioncloud/dragonfly/hotkeys/HotkeyController.kt
+++ src/main/java/net/inceptioncloud/dragonfly/hotkeys/HotkeyController.kt
@@ -11,36 +11,59 @@ object HotkeyController {
     @JvmStatic
     val hotkeys = mutableListOf<Hotkey>()
 
+    private val pressedKeys = HashMap<Hotkey, List<Int>>()
+
     init {
         println("Initializing hotkeys...")
-        hotkeys.add(
-            HotkeyTypeChat(
-                Keyboard.KEY_G,
-                Keyboard.KEY_LSHIFT,
-                1.0,
-                10.0,
-                Color(0x26de81),
-                true,
-                "Mitte Safe, alles Safe"
-            )
+        val test = HotkeyTypeChat(
+            Keyboard.KEY_G,
+            Keyboard.KEY_LSHIFT,
+            1.0,
+            10.0,
+            Color(0x26de81),
+            true,
+            "Mitte Safe, alles Safe"
         )
+        hotkeys.add(test)
+        pressedKeys.put(test, listOf(-1, -1))
     }
 
     @JvmStatic
     fun updateKeys() {
-        for(hotkey in hotkeys) {
-            if(hotkey.modifierKey == null) {
+
+        for (hotkey in hotkeys) {
+            if (hotkey.modifierKey == null) {
                 // Primary key only
-            }else {
-                if(hotkey.modifierKey == Keyboard.KEY_LSHIFT && GuiScreen.isShiftKeyDown && hotkey.key.isKeyPressed()) {
-                    if(hotkey is HotkeyTypeChat) {
-                        hotkey.direction = 1
-                    }
-                }else {
-                    if(hotkey is HotkeyTypeChat && !hotkey.transition.isAtStart) {
-                        hotkey.direction = -1
+            } else {
+
+                val primary = hotkey.key
+                val secondary = hotkey.modifierKey
+
+                if (secondary == Keyboard.KEY_LSHIFT && GuiScreen.isShiftKeyDown && !primary.isKeyPressed()) {
+                    pressedKeys[hotkey] = listOf(secondary, -1)
+                }
+
+                if (secondary == Keyboard.KEY_LSHIFT && GuiScreen.isShiftKeyDown && primary.isKeyPressed()) {
+                    val old = pressedKeys[hotkey]
+                    pressedKeys[hotkey] = listOf(old!![0], primary)
+                }
+
+                if (pressedKeys[hotkey]!![0] == secondary && pressedKeys[hotkey]!![0] == primary) {
+                    if (secondary == Keyboard.KEY_LSHIFT && GuiScreen.isShiftKeyDown && primary.isKeyPressed()) {
+
+                        if (hotkey is HotkeyTypeChat) {
+                            hotkey.direction = 1
+                        }
+
+                    } else {
+
+                        if (hotkey is HotkeyTypeChat && !hotkey.transition.isAtStart) {
+                            hotkey.direction = -1
+                        }
+
                     }
                 }
+
             }
         }
     }
