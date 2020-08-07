1cef16aaf36351727de1dadbaa264ef23a83f678
Verity <58147038+verityyt@users.noreply.github.com>
2020-08-07 21:22:27 +0200

    Publish new version

--- src/main/java/net/inceptioncloud/dragonfly/versioning/DragonflyVersion.kt
+++ src/main/java/net/inceptioncloud/dragonfly/versioning/DragonflyVersion.kt
@@ -15,7 +15,7 @@ object DragonflyVersion {
     /**
      * The version of this client.
      */
-    val localVersion = Version(1, 1, 2, 0)
+    val localVersion = Version(1, 1, 3, 0)
 
     /**
      * The version that is specified as the latest one, fetched lazily from the Inception Cloud
