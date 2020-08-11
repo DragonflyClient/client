1ce24342e73016a0935cbd51892cc6cf389c93ba
moritz <theincxption@gmail.com>
2020-08-12 00:38:10 +0200

    Documentation for InspectorApp.kt

--- src/main/java/net/inceptioncloud/dragonfly/engine/inspector/InspectorApp.kt
+++ src/main/java/net/inceptioncloud/dragonfly/engine/inspector/InspectorApp.kt
@@ -5,6 +5,16 @@ import javafx.stage.Stage
 import org.apache.logging.log4j.LogManager
 import tornadofx.*
 
+/**
+ * The main TornadoFx app that represents the inspector.
+ *
+ * The app specifies the [InspectorView] as the main view and the [InspectorStyle]
+ * as a stylesheet by calling the [App] constructor.
+ *
+ * This class is only referenced by [Inspector.launch] and does nothing more
+ * than initializing the stage and setting the [Inspector.stage] property in the
+ * [start] function.
+ */
 class InspectorApp : App(InspectorView::class, InspectorStyle::class) {
 
     override fun start(stage: Stage) {
