2cd7c714f685e31b3d0255e295c3b318c79c4f2f
moritz <theincxption@gmail.com>
2020-08-12 00:42:51 +0200

    Documentation for InspectorSubscriber.kt

--- src/main/java/net/inceptioncloud/dragonfly/engine/inspector/InspectorSubscriber.kt
+++ src/main/java/net/inceptioncloud/dragonfly/engine/inspector/InspectorSubscriber.kt
@@ -2,14 +2,27 @@ package net.inceptioncloud.dragonfly.engine.inspector
 
 import com.google.common.eventbus.Subscribe
 import net.inceptioncloud.dragonfly.event.control.KeyInputEvent
-import net.minecraft.client.gui.GuiScreen
-import org.lwjgl.input.Keyboard
+import net.inceptioncloud.dragonfly.subscriber.DefaultSubscribers
+import net.minecraft.client.gui.GuiScreen.Companion.isCtrlKeyDown
+import net.minecraft.client.gui.GuiScreen.Companion.isShiftKeyDown
+import org.lwjgl.input.Keyboard.KEY_I
 
+/**
+ * A [default subscriber][DefaultSubscribers] that uses the [KeyInputEvent] to open the
+ * inspector when the dedicated key combination is pressed.
+ *
+ * The default key combination (known from browsers) is `Ctrl+Shift+I`. As soon as the `I`
+ * key is pressed while the two modifiers are hold down, [Inspector.launch] is called to
+ * launch or re-open the inspector.
+ */
 object InspectorSubscriber {
+
     @Subscribe
     fun onKeyType(event: KeyInputEvent) {
-        if (event.press && event.key == Keyboard.KEY_I && GuiScreen.isCtrlKeyDown && GuiScreen.isShiftKeyDown) {
-            Inspector.launch()
+        with(event) {
+            if (press && key == KEY_I && isCtrlKeyDown && isShiftKeyDown) {
+                Inspector.launch()
+            }
         }
     }
 }
