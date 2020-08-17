7f5bd79b474a369867fcebc81b6d4b1fb971bd7a
incxption <theincxption@gmail.com>
2020-08-17 18:32:21 +0200

    :sparkles: Create account card widget

---
+++ src/main/java/net/inceptioncloud/dragonfly/apps/accountmanager/AccountCard.kt
@@ -0,0 +1,210 @@
+package net.inceptioncloud.dragonfly.apps.accountmanager
+
+import kotlinx.coroutines.*
+import net.inceptioncloud.dragonfly.Dragonfly
+import net.inceptioncloud.dragonfly.design.color.DragonflyPalette
+import net.inceptioncloud.dragonfly.engine.internal.*
+import net.inceptioncloud.dragonfly.engine.structure.IDimension
+import net.inceptioncloud.dragonfly.engine.structure.IPosition
+import net.inceptioncloud.dragonfly.engine.widgets.assembled.OutlineButton
+import net.inceptioncloud.dragonfly.engine.widgets.assembled.TextField
+import net.inceptioncloud.dragonfly.engine.widgets.primitive.Image
+import net.inceptioncloud.dragonfly.engine.widgets.primitive.Rectangle
+import net.inceptioncloud.dragonfly.overlay.toast.Toast
+import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.texture.DynamicTexture
+import net.minecraft.util.ResourceLocation
+
+class AccountCard(
+    val account: Account,
+    initializerBlock: (AccountCard.() -> Unit)? = null
+) : AssembledWidget<AccountCard>(initializerBlock), IPosition, IDimension {
+
+    override var x: Double by property(0.0)
+    override var y: Double by property(0.0)
+    override var width: Double = -1.0
+    override var height: Double by property(450.0)
+
+    var isSelected: Boolean by property(false)
+    var isExpired: Boolean by property(false)
+
+    override fun assemble(): Map<String, Widget<*>> = mapOf(
+        "container" to Rectangle(),
+        "expired-icon" to Image(),
+        "skull-shadow" to Rectangle(),
+        "skull" to Image(),
+        "name" to TextField(),
+        "email" to TextField(),
+        "secondary-button" to OutlineButton(), // logout button
+        "primary-button" to OutlineButton()    // switch/refresh button
+    )
+
+    override fun updateStructure() {
+        this.width = getCardWidth(account.displayName)
+
+        val accentColor = if (isSelected) DragonflyPalette.accentNormal else DragonflyPalette.foreground
+
+        "container"<Rectangle> {
+            x = this@AccountCard.x
+            y = this@AccountCard.y
+            width = this@AccountCard.width
+            height = this@AccountCard.height
+            color = accentColor.altered { alphaDouble = 0.2 }
+            outlineColor = accentColor
+            outlineStroke = 2.0
+        }
+
+        "expired-icon"<Image> {
+            if (isExpired) {
+                isVisible = true
+
+                width = 40.0
+                height = 40.0
+                x = this@AccountCard.x + this@AccountCard.width - width / 2.0
+                y = this@AccountCard.y - height / 2.0
+                resourceLocation = ResourceLocation("dragonflyres/icons/expired.png")
+            } else isVisible = false
+        }
+
+        val skull = "skull"<Image> {
+            width = 100.0
+            height = 100.0
+            x = this@AccountCard.x + (this@AccountCard.width - width) / 2.0
+            y = this@AccountCard.y + 30.0
+            resourceLocation = ResourceLocation("dragonflyres/icons/mainmenu/steve-skull.png")
+        }!!
+
+        "skull-shadow"<Rectangle> {
+            width = skull.width
+            height = skull.height
+            x = skull.x + 2.0
+            y = skull.y + 2.0
+            color = WidgetColor(0, 0, 0, 50)
+        }
+
+        val name = "name"<TextField> {
+            x = this@AccountCard.x
+            y = skull.y + skull.height + 20.0
+            width = this@AccountCard.width
+            adaptHeight = true
+            fontRenderer = Dragonfly.fontManager.defaultFont.fontRenderer(size = 60)
+            staticText = account.displayName
+            textAlignHorizontal = Alignment.CENTER
+            dropShadow = true
+        }!!.also { it.adaptHeight() }
+
+        "email"<TextField> {
+            x = this@AccountCard.x
+            y = name.y + name.height + 7.0
+            width = this@AccountCard.width
+            adaptHeight = true
+            fontRenderer = Dragonfly.fontManager.defaultFont.fontRenderer(size = 35)
+            staticText = censorEmail(account.email)
+            textAlignHorizontal = Alignment.CENTER
+            dropShadow = true
+        }!!.also { it.adaptHeight() }
+
+        val buttonMargin = 10.0
+
+        val secondaryButton = "secondary-button"<OutlineButton> {
+            width = this@AccountCard.width - buttonMargin * 2
+            height = 45.0
+            x = this@AccountCard.x + buttonMargin
+            y = this@AccountCard.y + this@AccountCard.height - buttonMargin - height
+
+            if (isSelected) {
+                text = "Refresh"
+            } else {
+                text = "Switch"
+                color = DragonflyPalette.accentNormal
+            }
+
+            onClick {
+                GlobalScope.launch(Dispatchers.IO) {
+                    if (isSelected) {
+                        if (account.refresh()) {
+                            Toast.queue("Access token refreshed", 500)
+                        } else {
+                            Toast.queue("Could not refresh token!", 500)
+                        }
+                    } else {
+                        val valid = if (isExpired) {
+                            Toast.queue("Refreshing access token...", 200)
+                            account.refresh()
+                        } else {
+                            account.validate()
+                        }
+
+                        if (valid) {
+                            Minecraft.getMinecraft().session = account.toSession()
+                            Toast.queue("Switched to account '${account.displayName}'", 500)
+                        } else {
+                            Toast.queue("Access to account '${account.displayName}' has expired!", 500)
+                        }
+                    }
+
+                    Minecraft.getMinecraft().addScheduledTask {
+                        Minecraft.getMinecraft().currentScreen.refresh()
+                    }
+                }
+            }
+        }!!
+
+        "primary-button"<OutlineButton> {
+            width = this@AccountCard.width - buttonMargin * 2
+            height = 45.0
+            x = this@AccountCard.x + buttonMargin
+            y = secondaryButton.y - height - buttonMargin
+            text = "Logout"
+
+            onClick {
+                GlobalScope.launch(Dispatchers.IO) {
+                    account.invalidate()
+                    Toast.queue("Account '${account.displayName}' has been logged out", 200)
+
+                    Minecraft.getMinecraft().addScheduledTask {
+                        Minecraft.getMinecraft().currentScreen.refresh()
+                    }
+                }
+            }
+        }
+
+        if (skull.dynamicTexture == null) {
+            GlobalScope.launch(Dispatchers.IO) {
+                val image = account.retrieveSkull() ?: return@launch
+                Minecraft.getMinecraft().addScheduledTask {
+                    skull.dynamicTexture = DynamicTexture(image)
+                }
+            }
+        }
+    }
+
+    override fun handleMousePress(data: MouseData) {
+        structure.forEach { it.value.handleMousePress(data) }
+    }
+
+    companion object {
+
+        /**
+         * Calculates the with of the [AccountCard] based on the [name] of the account.
+         */
+        fun getCardWidth(name: String): Double {
+            val fontRenderer = Dragonfly.fontManager.defaultFont.fontRenderer(size = 60)
+            return (fontRenderer.getStringWidth(name) + 20.0).coerceAtLeast(250.0)
+        }
+
+        /**
+         * Censors the email by only keeping the first three letters of the username and
+         * the provider and appending four wildcards to the username (eg. abc****@gmail.com).
+         */
+        fun censorEmail(email: String): String {
+            if ("@" !in email) return email
+
+            val provider = email.split("@").last()
+            val username = email.removeSuffix("@$provider").toCharArray()
+            val firstThree = username.take(3).joinToString("")
+
+            return "$firstThree****@$provider".toLowerCase()
+        }
+    }
+}
--- src/main/java/net/inceptioncloud/dragonfly/apps/accountmanager/AccountManagerUI.kt
+++ src/main/java/net/inceptioncloud/dragonfly/apps/accountmanager/AccountManagerUI.kt
@@ -1,17 +1,64 @@
 package net.inceptioncloud.dragonfly.apps.accountmanager
 
+import kotlinx.coroutines.*
+import net.inceptioncloud.dragonfly.Dragonfly
 import net.inceptioncloud.dragonfly.engine.internal.SizedImage
+import net.inceptioncloud.dragonfly.engine.widgets.primitive.Image
 import net.inceptioncloud.dragonfly.ui.screens.MainMenuUI
+import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.GuiScreen
+import net.minecraft.client.renderer.GlStateManager
+import net.minecraft.util.ResourceLocation
+import org.lwjgl.input.Keyboard
 import java.lang.Double.min
+import javax.swing.JOptionPane
 
-class AccountManagerUI(previousScreen: GuiScreen) : GuiScreen() {
+class AccountManagerUI(val previousScreen: GuiScreen) : GuiScreen() {
 
     override var backgroundImage: SizedImage? = MainMenuUI.splashImage
 
     override var customScaleFactor: () -> Double? = { min(mc.displayWidth / 1920.0, mc.displayHeight / 1080.0) }
 
-    override fun initGui() {
+    val accounts = AccountManagerApp.readFromLauncher()?.flatMap { listOf(it, it) } ?: listOf()
 
+    override fun initGui() {
+        + Image {
+            val aspectRatio = 384.0 / 76.0
+
+            resourceLocation = ResourceLocation("dragonflyres/logos/account-manager.png")
+            height = 70.0
+            width = height * aspectRatio
+            x = this@AccountManagerUI.width / 2.0 - width / 2.0
+            y = 70.0
+        } id "account-manager-logo"
+
+        if (accounts.isEmpty()) return
+
+        val space = 100.0
+        val totalWidth = accounts.sumByDouble { AccountCard.getCardWidth(it.displayName) } + space * (accounts.size - 1)
+        var currentX = width / 2.0 - totalWidth / 2.0
+
+        accounts.forEachIndexed { index, it ->
+            val accountCard = +AccountCard(it) {
+                x = currentX
+                y = 300.0
+                isSelected = Minecraft.getMinecraft().session.playerID == it.uuid.toString()
+            } id "account-$index"
+
+            GlobalScope.launch(Dispatchers.IO) {
+                accountCard.isExpired = !it.validate()
+            }
+
+            currentX += AccountCard.getCardWidth(it.displayName) + space
+        }
+    }
+
+    override fun keyTyped(typedChar: Char, keyCode: Int) {
+        if (keyCode == 1 && canManuallyClose) {
+            mc.displayGuiScreen(previousScreen)
+            return
+        }
+
+        super.keyTyped(typedChar, keyCode)
     }
 }
