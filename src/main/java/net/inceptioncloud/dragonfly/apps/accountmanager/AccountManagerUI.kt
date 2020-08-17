2b6e4c47a0c9c09385ac7df3b99f47d42aa370ea
incxption <theincxption@gmail.com>
2020-08-17 20:10:16 +0200

    :lipstick: Add back navigation widget

--- src/main/java/net/inceptioncloud/dragonfly/apps/accountmanager/AccountManagerUI.kt
+++ src/main/java/net/inceptioncloud/dragonfly/apps/accountmanager/AccountManagerUI.kt
@@ -1,17 +1,14 @@
 package net.inceptioncloud.dragonfly.apps.accountmanager
 
 import kotlinx.coroutines.*
-import net.inceptioncloud.dragonfly.Dragonfly
 import net.inceptioncloud.dragonfly.engine.internal.SizedImage
+import net.inceptioncloud.dragonfly.engine.widgets.assembled.BackNavigation
 import net.inceptioncloud.dragonfly.engine.widgets.primitive.Image
 import net.inceptioncloud.dragonfly.ui.screens.MainMenuUI
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.GuiScreen
-import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.util.ResourceLocation
-import org.lwjgl.input.Keyboard
 import java.lang.Double.min
-import javax.swing.JOptionPane
 
 class AccountManagerUI(val previousScreen: GuiScreen) : GuiScreen() {
 
@@ -19,10 +16,10 @@ class AccountManagerUI(val previousScreen: GuiScreen) : GuiScreen() {
 
     override var customScaleFactor: () -> Double? = { min(mc.displayWidth / 1920.0, mc.displayHeight / 1080.0) }
 
-    val accounts = AccountManagerApp.readFromLauncher()?.flatMap { listOf(it, it) } ?: listOf()
+    val accounts = AccountManagerApp.readFromLauncher() ?: listOf()
 
     override fun initGui() {
-        + Image {
+        +Image {
             val aspectRatio = 384.0 / 76.0
 
             resourceLocation = ResourceLocation("dragonflyres/logos/account-manager.png")
@@ -30,7 +27,13 @@ class AccountManagerUI(val previousScreen: GuiScreen) : GuiScreen() {
             width = height * aspectRatio
             x = this@AccountManagerUI.width / 2.0 - width / 2.0
             y = 70.0
-        } id "account-manager-logo"
+        } id "logo-account-manager"
+
+        +BackNavigation {
+            x = 10.0
+            y = this@AccountManagerUI.height - height - 10.0
+            gui(previousScreen)
+        } id "back-navigation"
 
         if (accounts.isEmpty()) return
 
