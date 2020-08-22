118ebf4da3d02134c256af75d190060c1c89b38b
incxption <theincxption@gmail.com>
2020-08-22 18:27:09 +0200

    :children_crossing: Add useful extension functions for events

--- src/main/java/net/inceptioncloud/dragonfly/event/Event.kt
+++ src/main/java/net/inceptioncloud/dragonfly/event/Event.kt
@@ -7,16 +7,10 @@ import net.inceptioncloud.dragonfly.Dragonfly
  */
 interface Event
 
-fun Event.post(): Event {
+/**
+ * Posts the event to the [Dragonfly.eventBus] returning the event itself.
+ */
+fun <T : Event> T.post(): T {
     Dragonfly.eventBus.post(this)
     return this
-}
-
-fun Cancellable.post(): Cancellable {
-    Dragonfly.eventBus.post(this)
-    return this
-}
-
-fun Cancellable.checkCancellation(): Boolean {
-    return isCancelled
 }
