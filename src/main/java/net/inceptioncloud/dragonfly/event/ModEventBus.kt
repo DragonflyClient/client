a8a57dbccf42f6234efac57bfc02e21d27c1c617
incxption <theincxption@gmail.com>
2020-08-22 18:18:04 +0200

    :building_construction: Convert ModEventBus to Kotlin

--- src/main/java/net/inceptioncloud/dragonfly/event/ModEventBus.kt
+++ src/main/java/net/inceptioncloud/dragonfly/event/ModEventBus.kt
@@ -1,44 +1,34 @@
-package net.inceptioncloud.dragonfly.event;
+package net.inceptioncloud.dragonfly.event
 
-import com.google.common.eventbus.*;
-import net.inceptioncloud.dragonfly.versioning.updater.ApplicationStartSubscriber;
-import org.apache.logging.log4j.LogManager;
+import com.google.common.eventbus.EventBus
+import net.inceptioncloud.dragonfly.versioning.updater.ApplicationStartSubscriber
+import org.apache.logging.log4j.LogManager
 
 /**
- * A custom {@link EventBus} for the InceptionCloud Mod Events.
+ * A custom [EventBus] for the InceptionCloud Mod Events.
  */
-public class ModEventBus extends EventBus {
-    /**
-     * Creates a new EventBus with the given {@link SubscriberExceptionHandler}.
-     */
-    public ModEventBus() {
-        super(new ModSubscriberExceptionHandler());
-
-        register(new ApplicationStartSubscriber());
-    }
+class ModEventBus : EventBus(ModSubscriberExceptionHandler()) {
 
     /**
-     * Registers all subscriber methods on {@code object} to receive events.
+     * Registers all subscriber methods on `object` to receive events.
      * Subscriber methods are selected and classified using this EventBus's
      * SubscriberFindingStrategy; the default strategy is the AnnotatedSubscriberFinder.
      *
      * @param object object whose subscriber methods should be registered.
      */
-    public void register(final Object object) {
-        super.register(object);
-
-        LogManager.getLogger().info("Registered Event Subscriber {}", object.getClass().getName());
+    override fun register(`object`: Any) {
+        super.register(`object`)
+        LogManager.getLogger().info("Registered Event Subscriber {}", `object`.javaClass.name)
     }
 
     /**
-     * Unregisters all subscriber methods on a registered {@code object}.
+     * Unregisters all subscriber methods on a registered `object`.
      *
      * @param object object whose subscriber methods should be unregistered.
      * @throws IllegalArgumentException if the object was not previously registered.
      */
-    @Override
-    public void unregister(final Object object) {
-        super.unregister(object);
+    override fun unregister(`object`: Any) {
+        super.unregister(`object`)
     }
 
     /**
@@ -46,14 +36,21 @@ public class ModEventBus extends EventBus {
      * successfully after the event has been posted to all subscribers, and
      * regardless of any exceptions thrown by subscribers.
      *
-     * <p>If no subscribers have been subscribed for {@code event}'s class, and
-     * {@code event} is not already a {@link DeadEvent}, it will be wrapped in a
+     *
+     * If no subscribers have been subscribed for `event`'s class, and
+     * `event` is not already a [DeadEvent], it will be wrapped in a
      * DeadEvent and reposted.
      *
      * @param event event to post.
      */
-    @Override
-    public void post(final Object event) {
-        super.post(event);
+    override fun post(event: Any) {
+        super.post(event)
+    }
+
+    /**
+     * Creates a new EventBus with the given [SubscriberExceptionHandler].
+     */
+    init {
+        register(ApplicationStartSubscriber())
     }
 }
