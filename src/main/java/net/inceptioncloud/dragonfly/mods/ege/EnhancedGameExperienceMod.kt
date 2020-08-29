0ee217593c975b8ee78e1dd8f2d87c21d8f5829c
incxption <theincxption@gmail.com>
2020-08-29 21:23:49 +0200

    :sparkles: Add option to force scale down resource pack icons

--- src/main/java/net/inceptioncloud/dragonfly/mods/ege/EnhancedGameExperienceMod.kt
+++ src/main/java/net/inceptioncloud/dragonfly/mods/ege/EnhancedGameExperienceMod.kt
@@ -1,15 +1,38 @@
 package net.inceptioncloud.dragonfly.mods.ege
 
 import net.inceptioncloud.dragonfly.mods.core.DragonflyMod
+import org.apache.logging.log4j.LogManager
+import java.awt.image.BufferedImage
 
 object EnhancedGameExperienceMod : DragonflyMod("Enhanced Game Experience") {
 
     @JvmStatic
     var tcpNoDelay by option(true)
+
+    @JvmStatic
+    var scaleResourcePackIcons by option(true)
+    var maximumPackIconSize by option(64)
+
     var disableExplicitGC by option(false)
 
     @JvmStatic
     fun tryExplicitGC() {
         if (!disableExplicitGC) System.gc()
+    }
+
+    @JvmStatic
+    fun scalePackImage(image: BufferedImage?): BufferedImage? {
+        if (image == null) return null
+        if (image.width <= maximumPackIconSize) return image
+
+        LogManager.getLogger().info("Scaling resource pack icon from ${image.width}x to ${maximumPackIconSize}x")
+
+        val smallImage = BufferedImage(maximumPackIconSize, maximumPackIconSize, BufferedImage.TYPE_INT_ARGB)
+        val graphics = smallImage.graphics
+
+        graphics.drawImage(image, 0, 0, maximumPackIconSize, maximumPackIconSize, null)
+        graphics.dispose()
+
+        return smallImage
     }
 }
