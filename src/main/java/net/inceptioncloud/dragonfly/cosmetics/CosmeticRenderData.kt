40cd05bccf5eef95cc521aa10deed88f55bbe077
incxption <theincxption@gmail.com>
2020-09-01 16:22:04 +0200

    :bulb: Write documentation for CosmeticRenderData.kt

--- src/main/java/net/inceptioncloud/dragonfly/cosmetics/CosmeticRenderData.kt
+++ src/main/java/net/inceptioncloud/dragonfly/cosmetics/CosmeticRenderData.kt
@@ -1,5 +1,9 @@
 package net.inceptioncloud.dragonfly.cosmetics
 
+/**
+ * Holds additional information that can be consumed by cosmetics
+ * during their rendering process.
+ */
 data class CosmeticRenderData(
     val limbSwing: Float,
     val limbSwingAmount: Float,
