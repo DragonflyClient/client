2da2ad3c052781b77f565603e1ecc0808b4a1c99
juli <julianwening4@gmail.com>
2020-09-17 21:55:09 +0200

    :mag: Add campaign to ideas platform link
    Update the url of the link leading to the dragonfly ideas platform in order to track analytics.

--- src/main/java/net/inceptioncloud/dragonfly/apps/IdeasPlatformApp.kt
+++ src/main/java/net/inceptioncloud/dragonfly/apps/IdeasPlatformApp.kt
@@ -4,5 +4,5 @@ import net.inceptioncloud.dragonfly.ui.taskbar.TaskbarApp
 
 object IdeasPlatformApp : TaskbarApp("Ideas Platform") {
 
-    override fun open() = url("https://ideas.playdragonfly.net")
+    override fun open() = url("https://ideas.playdragonfly.net/?utm_source=client&utm_medium=mod_link&utm_campaign=ideas_platform")
 }
