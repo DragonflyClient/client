94e558ea0091d5c55415fbdac94cf177f05447bd
incxption <theincxption@gmail.com>
2020-09-24 19:59:36 +0200

    :fire: Remove tooltip positions "Left" and "Right"

--- src/main/java/net/inceptioncloud/dragonfly/engine/tooltip/TooltipPosition.kt
+++ src/main/java/net/inceptioncloud/dragonfly/engine/tooltip/TooltipPosition.kt
@@ -14,21 +14,6 @@ enum class TooltipPosition {
         override fun TooltipWidget.arrowPoint1() = x - arrowSize to background!!.y
         override fun TooltipWidget.arrowPoint2() = x + arrowSize to background!!.y
         override fun TooltipWidget.arrowPoint3() = x to background!!.y - arrowSize
-    },
-    LEFT {
-        private val RoundedRectangle.midY get() = y + height / 2.0
-        private val RoundedRectangle.endX get() = x + width
-
-        override fun TooltipWidget.arrowPoint1() = background!!.endX to background!!.midY - arrowSize
-        override fun TooltipWidget.arrowPoint2() = background!!.endX to background!!.midY + arrowSize
-        override fun TooltipWidget.arrowPoint3() = background!!.endX + arrowSize to background!!.midY
-    },
-    RIGHT {
-        private val RoundedRectangle.midY get() = y + height / 2.0
-
-        override fun TooltipWidget.arrowPoint1() = background!!.x to background!!.midY - arrowSize
-        override fun TooltipWidget.arrowPoint2() = background!!.x to background!!.midY + arrowSize
-        override fun TooltipWidget.arrowPoint3() = background!!.x - arrowSize to background!!.midY
     };
 
     open fun TooltipWidget.arrowPoint1(): Pair<Double, Double> {
