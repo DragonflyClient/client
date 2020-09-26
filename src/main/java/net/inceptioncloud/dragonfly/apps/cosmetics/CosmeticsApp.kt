5c03dd484a1128247adc162050ab241d7b3b572b
incxption <theincxption@gmail.com>
2020-09-26 13:32:19 +0200

    :boom: Show NoCosmeticsUI if no Dragonfly account cosmetics are found

--- src/main/java/net/inceptioncloud/dragonfly/apps/cosmetics/CosmeticsApp.kt
+++ src/main/java/net/inceptioncloud/dragonfly/apps/cosmetics/CosmeticsApp.kt
@@ -1,9 +1,17 @@
 package net.inceptioncloud.dragonfly.apps.cosmetics
 
+import net.inceptioncloud.dragonfly.cosmetics.logic.CosmeticsManager
 import net.inceptioncloud.dragonfly.ui.taskbar.TaskbarApp
 import net.minecraft.client.Minecraft
 
 object CosmeticsApp : TaskbarApp("Cosmetics") {
 
-    override fun open() = gui(CosmeticsUI(Minecraft.getMinecraft().currentScreen))
+    override fun open() {
+        val cosmetics = CosmeticsManager.dragonflyAccountCosmetics
+        if (cosmetics?.isNotEmpty() == true) {
+            gui(CosmeticsUI(Minecraft.getMinecraft().currentScreen))
+        } else {
+            gui(NoCosmeticsUI(Minecraft.getMinecraft().currentScreen))
+        }
+    }
 }
