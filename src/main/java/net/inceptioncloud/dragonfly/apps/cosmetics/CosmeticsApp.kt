7a2547958b96e2e14829e309bd81043a0e963eba
incxption <theincxption@gmail.com>
2020-09-26 15:07:02 +0200

    :children_crossing: Show login modal when trying to access cosmetics without being logged in

--- src/main/java/net/inceptioncloud/dragonfly/apps/cosmetics/CosmeticsApp.kt
+++ src/main/java/net/inceptioncloud/dragonfly/apps/cosmetics/CosmeticsApp.kt
@@ -1,17 +1,19 @@
 package net.inceptioncloud.dragonfly.apps.cosmetics
 
+import net.inceptioncloud.dragonfly.Dragonfly
+import net.inceptioncloud.dragonfly.account.AuthenticationBridge
 import net.inceptioncloud.dragonfly.cosmetics.logic.CosmeticsManager
+import net.inceptioncloud.dragonfly.mc
 import net.inceptioncloud.dragonfly.ui.taskbar.TaskbarApp
 import net.minecraft.client.Minecraft
 
 object CosmeticsApp : TaskbarApp("Cosmetics") {
 
     override fun open() {
-        val cosmetics = CosmeticsManager.dragonflyAccountCosmetics
-        if (cosmetics?.isNotEmpty() == true) {
-            gui(CosmeticsUI(Minecraft.getMinecraft().currentScreen))
-        } else {
-            gui(NoCosmeticsUI(Minecraft.getMinecraft().currentScreen))
+        when {
+            Dragonfly.account == null -> AuthenticationBridge.showLoginModal()
+            CosmeticsManager.dragonflyAccountCosmetics?.isNotEmpty() == true -> gui(CosmeticsUI(mc.currentScreen))
+            else -> gui(NoCosmeticsUI(mc.currentScreen))
         }
     }
 }
