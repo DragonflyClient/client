494d0d27aeb8f0b0b5c94b19ef8fc4305cceadb6
incxption <theincxption@gmail.com>
2020-10-02 23:16:24 +0200

    :children_crossing: Handle issues when trying to start a session
    CU-8rt4wu

--- src/main/java/net/inceptioncloud/dragonfly/kernel/KernelAuthSubscriber.kt
+++ src/main/java/net/inceptioncloud/dragonfly/kernel/KernelAuthSubscriber.kt
@@ -3,6 +3,9 @@ package net.inceptioncloud.dragonfly.kernel
 import com.google.common.eventbus.Subscribe
 import net.inceptioncloud.dragonfly.event.dragonfly.DragonflyAuthEvent
 
+/**
+ * Listens to the [DragonflyAuthEvent] to start a connection to the Dragonfly Kernel server.
+ */
 object KernelAuthSubscriber {
 
     @Subscribe
