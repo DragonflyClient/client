149740a6a0052cd535fd388b3c6f48d038aafad9
incxption <theincxption@gmail.com>
2020-10-10 19:46:03 +0200

    :boom: Rework rounded rectangle drawing

--- src/main/java/net/inceptioncloud/dragonfly/engine/widgets/assembled/RoundedRectangle.kt
+++ src/main/java/net/inceptioncloud/dragonfly/engine/widgets/assembled/RoundedRectangle.kt
@@ -2,114 +2,59 @@ package net.inceptioncloud.dragonfly.engine.widgets.assembled
 
 import net.inceptioncloud.dragonfly.engine.internal.*
 import net.inceptioncloud.dragonfly.engine.structure.*
-import net.inceptioncloud.dragonfly.engine.widgets.primitive.Arc
-import net.inceptioncloud.dragonfly.engine.widgets.primitive.Rectangle
+import net.inceptioncloud.dragonfly.engine.utils.getCirclePoints
+import org.lwjgl.opengl.GL11.*
 
-/**
- * ## Rounded Rectangle Assembled Widget
- *
- * A simple rectangle with rounded corners.
- *
- * @property arc the size of the corner arc, specifies how rounded the corners are
- */
 class RoundedRectangle(
     initializerBlock: (RoundedRectangle.() -> Unit)? = null
-) : AssembledWidget<RoundedRectangle>(initializerBlock), IPosition, IDimension, IColor, IAlign {
+) : Widget<RoundedRectangle>(initializerBlock), IPosition, IDimension, IColor {
 
     override var x: Double by property(0.0)
     override var y: Double by property(0.0)
     override var width: Double by property(50.0)
     override var height: Double by property(50.0)
     override var color: WidgetColor by property(WidgetColor.DEFAULT)
-    override var horizontalAlignment: Alignment by property(Alignment.START)
-    override var verticalAlignment: Alignment by property(Alignment.START)
 
-    var arc: Double by property(0.0)
+    var arc: Double
+        set(value) {
+            topLeftArc = value
+            topRightArc = value
+            bottomLeftArc = value
+            bottomRightArc = value
+        }
+        get() = topLeftArc
 
-    init {
-        val (alignedX, alignedY) = align(x, y, width, height)
-        this.x = alignedX
-        this.y = alignedY
+    var topLeftArc by property(5.0)
+    var topRightArc by property(5.0)
+    var bottomRightArc by property(5.0)
+    var bottomLeftArc by property(5.0)
 
-        val smallest = width.coerceAtMost(height) / 2
-        this.arc = arc.coerceAtMost(smallest)
+    private var outlinePoints: List<Point> = listOf()
+
+    override fun stateChanged() {
+        outlinePoints = getCirclePoints(x + width - bottomRightArc, y + height - bottomRightArc, bottomRightArc, 0..90) +
+                getCirclePoints(x + bottomLeftArc, y + height - bottomLeftArc, bottomLeftArc, 90..180) +
+                getCirclePoints(x + topLeftArc, y + topLeftArc, topLeftArc, 180..270) +
+                getCirclePoints(x + width - topRightArc, y + topRightArc, topRightArc, 270..360) +
+                getCirclePoints(x + width - bottomRightArc, y + height - bottomRightArc, bottomRightArc, 0..0)
     }
 
-    override fun assemble(): Map<String, Widget<*>> {
-        return mapOf(
-            "left-top-edge" to Arc(),
-            "left-bottom-edge" to Arc(),
-            "right-bottom-edge" to Arc(),
-            "right-top-edge" to Arc(),
+    override fun render() {
+        color.glBindColor()
 
-            "rect-left" to Rectangle(),
-            "rect-right" to Rectangle(),
-            "rect-center" to Rectangle()
-        )
-    }
+        glPushMatrix()
+        glEnable(GL_BLEND)
+        glDisable(GL_TEXTURE_2D)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable(GL_LINE_SMOOTH)
+        glBegin(GL_TRIANGLE_FAN)
 
-    override fun updateStructure() {
-        structure.forEach { it.value.isVisible = width > arc && height > arc }
+        outlinePoints.forEach { glVertex2d(it.x, it.y) }
 
-        val spaceVertical = height - arc * 2
-        val spaceHorizontal = width - arc * 2
-
-        (structure["rect-left"] as Rectangle).apply {
-            x = this@RoundedRectangle.x
-            y = this@RoundedRectangle.y + arc
-            width = arc
-            height = spaceVertical
-        }
-        (structure["rect-right"] as Rectangle).apply {
-            x = this@RoundedRectangle.x + arc + spaceHorizontal
-            y = this@RoundedRectangle.y + arc
-            width = arc
-            height = spaceVertical
-        }
-        (structure["rect-center"] as Rectangle).apply {
-            x = this@RoundedRectangle.x + arc
-            y = this@RoundedRectangle.y
-            width = spaceHorizontal
-            height = this@RoundedRectangle.height
-        }
-
-        (structure["left-top-edge"] as Arc).apply {
-            x = this@RoundedRectangle.x
-            y = this@RoundedRectangle.y
-            start = 180
-            end = 270
-        }
-        (structure["left-bottom-edge"] as Arc).apply {
-            x = this@RoundedRectangle.x
-            y = this@RoundedRectangle.y + spaceVertical
-            start = 90
-            end = 180
-        }
-        (structure["right-top-edge"] as Arc).apply {
-            x = this@RoundedRectangle.x + spaceHorizontal
-            y = this@RoundedRectangle.y
-            start = 270
-            end = 360
-        }
-        (structure["right-bottom-edge"] as Arc).apply {
-            x = this@RoundedRectangle.x + spaceHorizontal
-            y = this@RoundedRectangle.y + spaceVertical
-            start = 0
-            end = 90
-        }
-
-        // changes the color of all base widgets
-        structure.values.forEach {
-            (it as IColor).color = this@RoundedRectangle.color
-        }
-        // sets the width and height of the arcs
-        structure
-            .filter { it.key.endsWith("edge") }
-            .forEach {
-                (it.value as Arc).apply {
-                    width = arc * 2
-                    height = arc * 2
-                }
-            }
+        glEnd()
+        glEnable(GL_TEXTURE_2D)
+        glDisable(GL_BLEND)
+        glDisable(GL_LINE_SMOOTH)
+        glPopMatrix()
     }
 }
