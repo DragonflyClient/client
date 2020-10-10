cb1ee53e466e4f58bc877398113c8868df52ec4e
incxption <theincxption@gmail.com>
2020-10-10 12:51:18 +0200

    :art: Add "fix ghost liquids" option to the Enhanced Game Experience mod & improve code structure

--- src/main/java/net/inceptioncloud/dragonfly/mods/ege/EnhancedGameExperienceMod.kt
+++ src/main/java/net/inceptioncloud/dragonfly/mods/ege/EnhancedGameExperienceMod.kt
@@ -16,6 +16,7 @@ object EnhancedGameExperienceMod : DragonflyMod("Enhanced Game Experience") {
     // bug fixes
     @JvmStatic var fixPlayerRotation by option(true)
     @JvmStatic var fixRidingHand by option(true)
+    @JvmStatic var fixGhostLiquids by option(true)
 
     /**
      * Calls [System.gc] if it is not [disabled][disableExplicitGC].
@@ -46,19 +47,49 @@ object EnhancedGameExperienceMod : DragonflyMod("Enhanced Game Experience") {
     }
 
     override fun publishControls() = listOf(
-        TitleControl("Enhancements",
-            "Customize enhancements that have been made to the Minecraft game in order to improve your game experience"),
-        BooleanControl(!::tcpNoDelay, "TCP no delay", "Improve your network connection speed by enabling this option"),
-        BooleanControl(!::scaleResourcePackIcons, "Scale resource pack icons",
-            "Decreases the size of resource pack icons to increase your performance and free memory"),
-        NumberControl(::maximumPackIconSize, "Maximum pack icon size",
-            "The maximum size that a resource pack icon is allowed to have, icons above this size are scaled down", min = 1.0, max = 128.0,
-            decimalPlaces = 0, formatter = { "${it}x$it" }),
-        BooleanControl(!::disableExplicitGC, "Disable explicit garbage collection",
-            "Prevents Minecraft from explicitly calling the JVM garbage collector to decrease world load time"),
-        TitleControl("Bug fixes", "Some issues that exist in Minecraft have been fixed by Dragonfly"),
-        BooleanControl(!::fixPlayerRotation, "Fix player rotation (crosshair sync)",
-            "Synchronizes the player look direction with their actual rotation and crosshair position which enhances the pvp experience"),
-        BooleanControl(!::fixRidingHand, "Fix hand while riding", "Fixes the right hand of the player which can look weird when the player is riding")
+        TitleControl(
+            "Enhancements",
+            "Customize enhancements that have been made to the Minecraft game in order to improve your game experience"
+        ),
+        BooleanControl(
+            !::tcpNoDelay,
+            "TCP no delay",
+            "Improve your network connection speed by enabling this option"
+        ),
+        BooleanControl(
+            !::scaleResourcePackIcons,
+            "Scale resource pack icons",
+            "Decreases the size of resource pack icons to increase your performance and free memory"
+        ),
+        NumberControl(
+            ::maximumPackIconSize,
+            "Maximum pack icon size",
+            "The maximum size that a resource pack icon is allowed to have, icons above this size are scaled down",
+            min = 1.0, max = 128.0, decimalPlaces = 0, formatter = { "${it}x$it" }
+        ),
+        BooleanControl(
+            !::disableExplicitGC,
+            "Disable explicit garbage collection",
+            "Prevents Minecraft from explicitly calling the JVM garbage collector to decrease world load time"
+        ),
+        TitleControl(
+            "Bug fixes",
+            "Some issues that exist in Minecraft have been fixed by Dragonfly"
+        ),
+        BooleanControl(
+            !::fixPlayerRotation,
+            "Fix player rotation (crosshair sync)",
+            "Synchronizes the player look direction with their actual rotation and crosshair position which enhances the pvp experience"
+        ),
+        BooleanControl(
+            !::fixRidingHand,
+            "Fix hand while riding",
+            "Fixes the right hand of the player which can look weird when the player is riding"
+        ),
+        BooleanControl(
+            !::fixGhostLiquids,
+            "Fix ghost liquids",
+            "Fixes an issues that caused the server to run out of sync with the client when placing lava or water on the floor"
+        )
     )
 }
